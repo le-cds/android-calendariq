9a323eebc82bcdd241669fe702bd2ad47fb8043b
le-cds <zephanya@web.de>
2020-12-20 14:41:16 +0100

    #18 Have Nagivation framework manage the action bar.

--- calendariq/src/main/java/net/hypotenubel/calendariq/ui/main/MainActivity.java
+++ calendariq/src/main/java/net/hypotenubel/calendariq/ui/main/MainActivity.java
@@ -3,18 +3,21 @@ package net.hypotenubel.calendariq.ui.main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import net.hypotenubel.calendariq.R;
 import net.hypotenubel.calendariq.util.Utilities;
 
 public class MainActivity extends AppCompatActivity {
 
-    /** Log tag used to log log messages in a logging fashion. */
     private static final String LOG_TAG = Utilities.logTag(MainActivity.class);
 
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    // Lifecycle
+    /** Used for having the Navigation framework manage the action bar. */
+    private AppBarConfiguration appBarConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
         // Set up the view and our action bar
         setContentView(R.layout.main_activity);
         setSupportActionBar(findViewById(R.id.mainActivity_appBar));
+
+        // Make the Navigation framework manage the action bar
+        NavController navController = getNavHostFragment().getNavController();
+        appBarConfig = new AppBarConfiguration.Builder(R.id.calendarListFragment).build();
+        Toolbar toolbar = findViewById(R.id.mainActivity_appBar);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavHostFragment navHostFragment = getNavHostFragment();
+        NavController navController = navHostFragment.getNavController();
+        return NavigationUI.navigateUp(navController, appBarConfig)
+                || super.onSupportNavigateUp();
+    }
+
+    private NavHostFragment getNavHostFragment() {
+        return (NavHostFragment) getSupportFragmentManager().findFragmentById(
+                R.id.mainActivity_fragmentContainer);
+    }
+
 
 }
