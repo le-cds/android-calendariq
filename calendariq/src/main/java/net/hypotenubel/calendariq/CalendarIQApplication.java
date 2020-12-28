4a3d614ae3424eada91538a268348f85edad2f91
le-cds <zephanya@web.de>
2020-12-28 20:32:31 +0100

    #22 Add and configure Hilt and Dagger.

---
+++ calendariq/src/main/java/net/hypotenubel/calendariq/CalendarIQApplication.java
@@ -0,0 +1,9 @@
+package net.hypotenubel.calendariq;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class CalendarIQApplication extends Application {
+}
