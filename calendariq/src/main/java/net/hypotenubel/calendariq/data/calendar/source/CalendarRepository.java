d3340e9a502b717be5fd7578d34c3da310ec7c20
le-cds <zephanya@web.de>
2021-01-01 17:38:11 +0100

    #22 Inject calendar view model and introduce repository.

---
+++ calendariq/src/main/java/net/hypotenubel/calendariq/data/calendar/source/CalendarRepository.java
@@ -0,0 +1,121 @@
+package net.hypotenubel.calendariq.data.calendar.source;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import net.hypotenubel.calendariq.data.Preferences;
+import net.hypotenubel.calendariq.data.calendar.model.CalendarDescriptor;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+/**
+ * Repository for accessing calendars and managing which ones are active and which ones are not.
+ */
+public class CalendarRepository {
+
+    private final Context context;
+
+    private final ICalendarSource calendarSource;
+
+    /** Live data for available calendars. Updated upon refresh. */
+    private final MutableLiveData<List<CalendarDescriptor>> availableCalendars
+            = new MutableLiveData<>();
+
+    /**
+     * Creates a new instance that retrieves calendars from the given calendar source.
+     */
+    @Inject
+    public CalendarRepository(@ApplicationContext Context context, ICalendarSource calendarSource) {
+        this.context = context;
+        this.calendarSource = calendarSource;
+        refreshAvailableCalendars();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Accessors
+
+    /**
+     * Returns the calendars available in the system.
+     */
+    public LiveData<List<CalendarDescriptor>> getAvailableCalendars() {
+        return availableCalendars;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Calendar Loading
+
+    /**
+     * Triggers a refresh of all available calendars and updates the associated live data.
+     */
+    public void refreshAvailableCalendars() {
+        // Fire off a thread that loads a new list of calendars
+        new Thread(() -> {
+            // Load available calendars
+            List<CalendarDescriptor> calList = calendarSource.getAvailableCalendars();
+
+            // Sort the list so that the UI won't have to
+            Collections.sort(calList);
+
+            // Set the calendar activity flags
+            loadActiveCalendarIds(calList);
+
+            // Update live data
+            availableCalendars.postValue(calList);
+        }).start();
+    }
+
+    /**
+     * Loads the set of active calendars and applies them to the given set of calendar descriptor
+     * instances accordingly. If we haven't loaded any calendars yet, this method loads activity
+     * flags from the preferences. Otherwise, it extracts IDs of active calendars from our current
+     * list and applies them to the new list. It it the latter which supports refreshing the
+     * calendar list.
+     */
+    private void loadActiveCalendarIds(List<CalendarDescriptor> calList) {
+        Set<Integer> activeCalIds;
+
+        // Obtain the active calendar IDs from preferences or current list of calendars
+        if (availableCalendars.getValue() == null) {
+            activeCalIds = Preferences.ACTIVE_CALENDARS.loadIntSet(context);
+        } else {
+            activeCalIds = new HashSet<>();
+            for (CalendarDescriptor cal : availableCalendars.getValue()) {
+                if (cal.isActive()) {
+                    activeCalIds.add(cal.getId());
+                }
+            }
+        }
+
+        // Go through the calendars and set their activity flags
+        for (CalendarDescriptor cal : calList) {
+            cal.setActive(activeCalIds.contains(cal.getId()));
+        }
+    }
+
+    /**
+     * Stores the IDs of active calendars to our preferences.
+     */
+    public void storeActiveCalendarIds() {
+        if (availableCalendars.getValue() != null) {
+            // Build a set of IDs of our active calendars
+            Set<Integer> activeCalendarIds = new HashSet<>();
+            for (CalendarDescriptor cal : availableCalendars.getValue()) {
+                if (cal.isActive()) {
+                    activeCalendarIds.add(cal.getId());
+                }
+            }
+
+            Preferences.ACTIVE_CALENDARS.storeIntSet(context, activeCalendarIds);
+        }
+    }
+
+}
--- calendariq/src/main/java/net/hypotenubel/calendariq/ui/main/calendar/CalendarViewModel.java
+++ calendariq/src/main/java/net/hypotenubel/calendariq/ui/main/calendar/CalendarViewModel.java
@@ -1,115 +1,44 @@
 package net.hypotenubel.calendariq.ui.main.calendar;
 
-import android.app.Application;
-
-import androidx.lifecycle.AndroidViewModel;
+import androidx.hilt.lifecycle.ViewModelInject;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
-import net.hypotenubel.calendariq.data.Preferences;
 import net.hypotenubel.calendariq.data.calendar.model.CalendarDescriptor;
-import net.hypotenubel.calendariq.data.calendar.source.ICalendarSource;
-import net.hypotenubel.calendariq.util.Utilities;
+import net.hypotenubel.calendariq.data.calendar.source.CalendarRepository;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * View model for the list of calendars. The activity state of the calendars is automatically
- * synchronised with the preferences.
+ * synchronised with the preferences. This is basically just a bit of glue code between the
+ * {@link CalendarRepository} and the {@link androidx.recyclerview.widget.RecyclerView}.
  */
-public class CalendarViewModel extends AndroidViewModel {
+public class CalendarViewModel extends ViewModel {
 
-    // TODO Make injectable and inject the ICalendarSource
+    // TODO Move the calendar account logic from the adapter to this view model. This is where it
+    //      belongs
 
-    /** List of calendars. */
-    private final MutableLiveData<List<CalendarDescriptor>> calendars = new MutableLiveData<>();
+    private final CalendarRepository calendarRepository;
 
     /**
      * Creates a new instance in the given application context.
      */
-    public CalendarViewModel(Application application) {
-        super(application);
-        refresh();
+    @ViewModelInject
+    public CalendarViewModel(CalendarRepository calendarRepository) {
+        this.calendarRepository = calendarRepository;
     }
 
-    /**
-     * Returns a {@link LiveData} object that contains a list of active calendars.
-     *
-     * @return list of calendars.
-     */
     public LiveData<List<CalendarDescriptor>> getCalendars() {
-        return calendars;
+        return calendarRepository.getAvailableCalendars();
     }
 
-    /**
-     * Refreshes our list of calendars.
-     */
     public void refresh() {
-        // Fire off a thread that loads a new list of calendars
-        new Thread(() -> {
-            // Load available calendars
-            ICalendarSource provider = Utilities.obtainCalendarProvider(getApplication());
-            List<CalendarDescriptor> calList = provider.getAvailableCalendars();
-
-            // Sort the list so that the UI won't have to
-            Collections.sort(calList);
-
-            // Set the calendar activity flags
-            loadActiveCalendarIds(calList);
-
-            // Update live data
-            calendars.postValue(calList);
-        }).start();
+        calendarRepository.refreshAvailableCalendars();
     }
 
-    /**
-     * Loads the set of active calendars and applies them to the given set of calendar descriptor
-     * instances accordingly. If we haven't loaded any calendars yet, this method loads activity
-     * flags from the preferences. Otherwise, it extracts IDs of active calendars from our current
-     * list and applies them to the new list. It it the latter which supports refreshing the
-     * calendar list.
-     */
-    private void loadActiveCalendarIds(List<CalendarDescriptor> calList) {
-        Set<Integer> activeCalIds;
-
-        // Obtain the active calendar IDs from preferences or current list of calendars
-        if (calendars.getValue() == null) {
-            activeCalIds = Preferences.ACTIVE_CALENDARS.loadIntSet(getApplication());
-        } else {
-            activeCalIds = new HashSet<>();
-            for (CalendarDescriptor cal : calendars.getValue()) {
-                if (cal.isActive()) {
-                    activeCalIds.add(cal.getId());
-                }
-            }
-        }
-
-        // Go through the calendars and set their activity flags
-        for (CalendarDescriptor cal : calList) {
-            cal.setActive(activeCalIds.contains(cal.getId()));
-        }
-    }
-
-    /**
-     * Stores the IDs of active calendars to our preferences.
-     */
     public void storeActiveCalendarIds() {
-        if (calendars == null || calendars.getValue() == null) {
-            return;
-        }
-
-        // Build a set of IDs of our active calendars
-        Set<Integer> activeCalendarIds = new HashSet<>();
-        for (CalendarDescriptor cal : calendars.getValue()) {
-            if (cal.isActive()) {
-                activeCalendarIds.add(cal.getId());
-            }
-        }
-
-        Preferences.ACTIVE_CALENDARS.storeIntSet(getApplication(), activeCalendarIds);
+        calendarRepository.storeActiveCalendarIds();
     }
 
 }
