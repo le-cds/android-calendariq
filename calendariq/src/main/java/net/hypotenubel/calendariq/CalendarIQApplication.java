4cc548daf5de8e02970a1dc3303814603819f706
le-cds <zephanya@web.de>
2021-01-03 13:55:55 +0100

    #22 Enable DI for WorkManager.

--- calendariq/src/main/java/net/hypotenubel/calendariq/CalendarIQApplication.java
+++ calendariq/src/main/java/net/hypotenubel/calendariq/CalendarIQApplication.java
@@ -2,8 +2,28 @@ package net.hypotenubel.calendariq;
 
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+import androidx.hilt.work.HiltWorkerFactory;
+import androidx.work.Configuration;
+
+import javax.inject.Inject;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
-public class CalendarIQApplication extends Application {
+public class CalendarIQApplication extends Application implements Configuration.Provider {
+
+    // Configure dependency injection for use with WorkManager
+
+    @Inject
+    HiltWorkerFactory workerFactory;
+
+    @NonNull
+    @Override
+    public Configuration getWorkManagerConfiguration() {
+        return new Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build();
+    }
+
 }
